f990b8ffc40477a9e6a19ee68e2659af18017f87
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-02-15 14:33:47 -0400

    Add connection to firebase cloud store and setup realtime monitoring

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
+require("dotenv").config();
 const express = require("express");
+const Firestore = require("@google-cloud/firestore");
 
 const app = express();
+const jsonParser = express.json();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Everything is OK!"
-  });
+const db = new Firestore({
+  projectId: process.env.FIRESTORE_PROJECT_ID,
+  credentials: {
+    private_key: process.env.FIRESTORE_PRIVATE_KEY,
+    client_email: process.env.FIRESTORE_CLIENT_EMAIL
+  }
+});
+
+//test the listener here
+let query = db.collection("bids");
+let unsubscribe = query.onSnapshot(qSnap => {
+  let changes = qSnap.docChanges();
+  for (let change of changes) {
+    console.log(`A document was ${change.type}`);
+    console.log(change.doc.data());
+  }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    let query = db.collection("bids");
+    let snapshot = await query.get();
+    const results = [];
+    snapshot.forEach(docSnap => results.push(docSnap.data()));
+    res.json(results);
+  } catch (err) {
+    res.status(500).send(`Something went terribly wrong! - ${err}`);
+  }
+});
+
+app.post("/", jsonParser, async (req, res) => {
+  let bids = db.collection("bids");
+  try {
+    let bidRef = await bids.add(req.body);
+    let newBid = await bidRef.get();
+    res.status(201).json(newBid.data());
+  } catch (err) {
+    res.status(500).send(`Something is really wrong here: ${err}`);
+  }
 });
 
 app.listen(9000, () => {
